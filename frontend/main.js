6ba3b6afd7f78f25ce79db4d62ad8b8142c53e1f
dekoska <zosiadekowska@gmail.com>
2025-01-24 21:49:46 +0100

    maly blad

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ import { getAccessToken } from './js/auth.js';
 import { queryCreatorInfo } from './js/creator-info.js';
 import { setupGenerateButton } from './js/generate-brainroot.js';
 import { setupUploadButton } from './js/content-video-upload.js';
-import { generateVideo } from './generate-brainroot.js';
+import { generateVideo } from './js/generate-brainroot.js';
 
 let accessToken = null;
 
